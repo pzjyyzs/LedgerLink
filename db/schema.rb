1300bf221f1e4fd46facd1ef3372aa7284718d33
pzjyyzs <463669395@qq.com>
2024-07-15 22:57:42 +0800

    分页

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_14_091859) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_15_142132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "amount"
+    t.text "notes"
+    t.bigint "tags_id", array: true
+    t.datetime "happend_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -24,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_14_091859) do
   create_table "validation_codes", force: :cascade do |t|
     t.string "email"
     t.integer "kind", default: 1, null: false
+    t.string "code", limit: 100
     t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
