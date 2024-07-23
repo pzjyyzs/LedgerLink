2e028cc1fcbee688dad70187f4b352f32a4c3215
pzjyyzs <463669395@qq.com>
2024-07-23 16:11:39 +0800

    宿主机里部署

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/', to: 'home#index'
+
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
